f545d2bd109fe440d62a26137907578cf9e498e6
Guang Ya Liu <liugya@cn.ibm.com>
2016-06-21 14:25:26 +0800

    Updated set up.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 unset LANG
 
 npm run build-assets
+rm -rf /opt/mesosphere/active/dcos-ui/usr/*
+cp -r dist/* /opt/mesosphere/active/dcos-ui/usr/
 
-#rm -rf /opt/mesosphere/active/dcos-ui/usr/*
-
-#cp -r dist/* /opt/mesosphere/active/dcos-ui/usr/
